148cf8f903d43457e9aaea929c864b6cb8eddaa6
przemek milke <theblimek@gmail.com>
2024-03-05 10:07:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string y;
+            float z;
+
+            x = 10;
+            y = "Szkoła Dotneta";
+            z = 10.5f;
+
+            Console.WriteLine($" {x}, {y}, {z}");
+            Console.ReadLine();
+        }
+    }
+}
